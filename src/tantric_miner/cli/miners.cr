dc489b351c0bef5c96a88bee127c3ca122966808
Priyavadan Kumar <priyavadan@gmail.com>
2018-03-15 23:57:59 -0500

    clean up

--- src/tantric_miner/cli/miners.cr
+++ src/tantric_miner/cli/miners.cr
@@ -7,17 +7,6 @@ module TantricMiner
       extend self
 
       def run
-        begin
-          OptionParser.parse! do |parser|
-            parser.banner = "Usage: salute [arguments]"
-
-            parser.on("-h", "--help", "Show this help") { puts parser }
-          end
-        rescue OptionParser::InvalidOption
-          puts "Invalid option supplied, run 'tm install -h' to see the valid options"
-          exit 1
-        end
-
         MINERS::CCMINER.install
       end
     end
